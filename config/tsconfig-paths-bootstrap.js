8fed0b1832607b0157466e52eacafdff0f855fab
Lucas Silva <lucas.silva@grupocriar>
2026-01-09 17:00:42 -0300

    fix dockerfile

--- config/tsconfig-paths-bootstrap.js
+++ config/tsconfig-paths-bootstrap.js
@@ -1,9 +1,19 @@
 const tsConfig = require("./tsconfig.json");
 const tsConfigPaths = require("tsconfig-paths");
+const path = require("path");
 
-const baseUrl = "../dist"; // Adjusted because it's now inside config/
+const baseUrl = path.join(__dirname, "../dist");
+
+// Em produção, os arquivos estão em 'dist/' e não mais em 'src/'
+// Precisamos remover o prefixo 'src/' dos mapeamentos de caminhos
+const paths = {};
+Object.keys(tsConfig.compilerOptions.paths).forEach((key) => {
+  paths[key] = tsConfig.compilerOptions.paths[key].map((p) =>
+    p.replace(/^src\//, "./")
+  );
+});
 
 tsConfigPaths.register({
   baseUrl,
-  paths: tsConfig.compilerOptions.paths,
+  paths: paths,
 });
